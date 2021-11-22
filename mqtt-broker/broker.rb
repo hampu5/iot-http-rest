2bfcea19fe0f6f35fa47c64e4673401caee5fbc6
hampu5 <halu1701@student.miun.se>
2021-11-22 23:32:05 +0100

    MQTT: Doing best to try Ruby, Might switch to Toit instead.

--- mqtt-broker/broker.rb
+++ mqtt-broker/broker.rb
@@ -9,6 +9,7 @@ puts "Listening on #{host}:#{port}"
 loop do
     client = server.accept
     data = client.recv(32) # 32 Bytes, can be less
+    puts client.addr
     puts data
     client.puts "Your data: #{data}"
     client.close
