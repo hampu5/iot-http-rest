c3bffe643397ac6676337a8c5cbac3c4149f1a74
hampu5 <halu1701@student.miun.se>
2021-11-23 19:19:03 +0100

    Edited client.js a bit, removed old code.

--- mqtt-broker/client.js
+++ mqtt-broker/client.js
@@ -1,23 +1,4 @@
-import net from 'net'
 import mqtt from 'mqtt'
-
-// const socket = net.connect({port:1883, host:'localhost'}, () => {
-//     console.log('Connected to Server!')
-//     socket.write('Hello World!')
-
-//     socket.on('data', (data) => {
-//         console.log(data.toString())
-//         socket.end()
-//     })
-
-//     socket.on('end', function() {
-//         socket.end()
-//     })
-// })
-
-// socket.on('error', (err) => {
-//     console.error(err)
-// })
 
 const client  = mqtt.connect({port:1883, host:'localhost'})
 
