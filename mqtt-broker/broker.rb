307fd5a81123dacf4ba5a1e4f3b6d1db027e0604
hampu5 <halu1701@student.miun.se>
2021-11-23 16:34:47 +0100

    MQTT Connect works now! Yay!

--- mqtt-broker/broker.rb
+++ mqtt-broker/broker.rb
@@ -8,11 +8,56 @@ puts "Listening on #{host}:#{port}"
 
 loop do
     client = server.accept
-    data = client.recv(32) # 32 Bytes, can be less
-    puts client.addr
-    puts data
-    client.puts "Your data: #{data}"
-    client.close
+    data_string = client.recv(64) # 32 Bytes, can be less
+    data = data_string.bytes
+
+    counter = 0
+
+    # First Byte
+    packet_type = data[counter] >> 4
+    dup_flag = (0x0F & data[counter]) >> 3
+    qos_level = (0b00000111 & data[counter]) >> 1
+    retain = (0b00000001)
+    counter += 1
+
+    # Second Byte
+    remaining_length = data[counter]
+    counter += 1
+
+    # Third and Fourth Bytes (length in connect/publish, ID in subscribe/unsubscribe)
+    msb = data[counter]
+    counter += 1
+    lsb = data[counter]
+    total_length = (msb << 8) | lsb
+    counter += 1
+
+    # Optional Header data (protocol name in Connect)
+    header_data = ''
+    for i in 0..total_length do
+        header_data += data_string[counter]
+        counter += 1
+    end
+
+    # Protocol version (in connect)
+    protocol_version = data[counter]
+    counter += 1
+
+    # Flags (in connect)
+    flags = data[counter]
+
+    # Keep alive (in connect)
+    keep_alive_msb = data[counter]
+    counter += 1
+    keep_alive_lsb = data[counter]
+    keep_alive = (keep_alive_msb << 8) | keep_alive_lsb
+    counter += 1
+
+    puts keep_alive
+    # puts client.addr
+    # puts data
+    response = [0x20, 0x02, 0x00, 0x00].pack('C*')
+    client.puts response
+    # client.close
 end
 
 socket.close
--- mqtt-broker/client.js
+++ mqtt-broker/client.js
@@ -1,19 +1,36 @@
 import net from 'net'
+import mqtt from 'mqtt'
 
-const socket = net.connect({port:1883, host:'localhost'}, () => {
-    console.log('Connected to Server!')
-    socket.write('Hello World!')
+// const socket = net.connect({port:1883, host:'localhost'}, () => {
+//     console.log('Connected to Server!')
+//     socket.write('Hello World!')
 
-    socket.on('data', (data) => {
-        console.log(data.toString())
-        socket.end()
-    })
+//     socket.on('data', (data) => {
+//         console.log(data.toString())
+//         socket.end()
+//     })
 
-    socket.on('end', function() {
-        socket.end()
-    })
+//     socket.on('end', function() {
+//         socket.end()
+//     })
+// })
+
+// socket.on('error', (err) => {
+//     console.error(err)
+// })
+
+const client  = mqtt.connect({port:1883, host:'localhost'})
+
+client.on('connect', function () {
+    // client.subscribe('presence', function (err) {
+    //     if (!err) {
+    //         client.publish('presence', 'Hello mqtt')
+    //     }
+    // })
 })
 
-socket.on('error', (err) => {
-    console.error(err)
+client.on('message', function (topic, message) {
+    // message is Buffer
+    // console.log(message.toString())
+    // client.end()
 })
