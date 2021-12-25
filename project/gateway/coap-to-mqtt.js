2924205bd233bc7aeb1c2692f7e6b53c3fa86687
hampu5 <halu1701@student.miun.se>
2021-12-25 02:03:46 +0100

    Everything works, from coap server to the coap-to-mqtt, to the broker, to a mqtt client (which will be a UI later).

--- project/gateway/coap-to-mqtt.js
+++ project/gateway/coap-to-mqtt.js
@@ -1,12 +1,14 @@
 import mqtt from 'mqtt'
 import coapClient from './coapClient.js'
 
-// CoAP Client
-
-const client = new coapClient()
 
 const remotePort = '5683'
 const remoteAddress = 'localhost'
+const brokerPort = '1883'
+const brokerAddress = 'localhost'
+
+const client = new coapClient()
+const mqttClient  = mqtt.connect({ host: brokerAddress, port: brokerPort })
 
 function fetchValue() {
     setTimeout(() => {
@@ -23,17 +25,15 @@ client.onConnect(() => {
 
 client.onMessage((payload, statusMessage) => {
     console.log(statusMessage + `: ${payload}`)
-    client.publish('sensor', payload)
+    mqttClient.publish('sensor', payload)
 })
 
 client.connect(remotePort, remoteAddress)
 
 // MQTT Client
 
-const client  = mqtt.connect(remoteAddress)
-
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end() // Maybe remove this
-})
+// mqttClient.on('message', function (topic, message) {
+//   // message is Buffer
+//   console.log(message.toString())
+//   client.end() // Maybe remove this
+// })
