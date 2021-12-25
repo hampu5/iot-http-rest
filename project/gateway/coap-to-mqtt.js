8a268d3da23f7fb2d05dcb66e4f434d406578f88
hampu5 <halu1701@student.miun.se>
2021-12-25 20:15:45 +0100

    Works from the piZW, with blocked dns traffic

--- project/gateway/coap-to-mqtt.js
+++ project/gateway/coap-to-mqtt.js
@@ -3,29 +3,31 @@ import coapClient from './coapClient.js'
 
 
 const remotePort = '5683'
-const remoteAddress = 'localhost'
+const remoteAddress = '192.168.1.33'
 const brokerPort = '1883'
 const brokerAddress = 'localhost'
+
+let topic = 'blocked'
 
 const client = new coapClient()
 const mqttClient  = mqtt.connect({ host: brokerAddress, port: brokerPort })
 
 function fetchValue() {
     setTimeout(() => {
-        client.request('GET', 'sensor')
+        client.request('GET', topic)
         fetchValue()
     }, 3000)
 }
 
 client.onConnect(() => {
     console.log('Connected to CoAP Server')
-    client.request('GET', 'sensor')
+    client.request('GET', topic)
     fetchValue()
 })
 
 client.onMessage((payload, statusMessage) => {
     console.log(statusMessage + `: ${payload}`)
-    mqttClient.publish('sensor', payload)
+    mqttClient.publish(topic, payload)
 })
 
 client.connect(remotePort, remoteAddress)
